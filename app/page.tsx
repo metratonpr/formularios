bd4e1dcd7a3b5871136849879f5111032870d8f8
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2026-04-22 18:38:22 +0000

    Botoes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client'
-import React, { useState } from "react";
-
+import React, { useState } from "react"; ''
 export default function Home() {
   // Declarar as variaveis
   const [nome, setNome] = useState("")
@@ -15,11 +14,20 @@ export default function Home() {
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <form action="" method="" onSubmit={enviarFormulario}>
-          <input type="text"
-            placeholder="Digite o seu Nome"
-            value={nome}
-               onChange={(e) => setNome(e.target.value)}
-          />
+          <div>
+            <label htmlFor="nome">Nome</label>
+            <input type="text"
+              placeholder="Digite o seu Nome"
+              value={nome}
+              onChange={(e) => setNome(e.target.value)}
+              name="nome"
+              id="nome"
+            />
+          </div>
+          <div>
+          <button type="submit">Enviar</button>
+          <button type="reset">Limpar</button>
+          </div>
         </form>
       </main>
     </div>
