2d6e1ea2d503fdcd96b899bd1177b1ff6ca7ecc0
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2026-04-22 19:10:54 +0000

    Data de Nascimento

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,12 @@ import React, { useState } from "react"; ''
 export default function Home() {
   // Declarar as variaveis
   const [nome, setNome] = useState("")
+  const [email, setEmail] = useState("")
+  const [idade, setIdade] = useState("")
+  const [nascimento, setNascimento] = useState("")
+  const [cnh, setCNH] = useState(false)
+  const [altura, setAltura] = useState("")
+
   // Funções
   function enviarFormulario(e: React.FormEvent) {
     // React.FormEvent
@@ -25,10 +31,41 @@ export default function Home() {
             />
           </div>
           <div>
-          <button type="submit">Enviar</button>
-          <button type="reset">Limpar</button>
+            <label htmlFor="email">E-mail</label>
+            <input type="email"
+              placeholder="Digite o seu E-mail"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              name="email"
+              id="email"
+            />
+          </div>
+          <div>
+            <label htmlFor="idade">Idade</label>
+            <input type="number"
+              placeholder="Digite o seu Idade"
+              value={idade}
+              onChange={(e) => setIdade(e.target.value)}
+              name="idade"
+              id="idade"
+            />
+          </div>
+          <div>
+            <label htmlFor="nascimento">Data de Nascimento</label>
+            <input type="date"
+              placeholder="Digite a sua data de Nascimento"
+              value={nascimento}
+              onChange={(e) => setNascimento(e.target.value)}
+              name="nascimento"
+              id="nascimento"
+            />
+          </div>
+          <div>
+            <button type="submit">Enviar</button>
+            <button type="reset">Limpar</button>
           </div>
         </form>
+        <h3>{nome}</h3>
       </main>
     </div>
   );
